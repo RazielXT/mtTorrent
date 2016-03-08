a29e0c1e4d3d802c0918f0506f3d819890c2d0c5
Marek Tomco <marek.tomco@eset.sk>
2016-03-08 13:17:22 +0100

    rename server to hostname

--- mtTorrent/Network.h
+++ mtTorrent/Network.h
@@ -12,10 +12,10 @@ using boost::asio::ip::udp;
 namespace ssl = boost::asio::ssl;
 typedef ssl::stream<tcp::socket> ssl_socket;
 
-void openSslSocket(ssl_socket& sock, tcp::resolver& resolver, const char* server);
-std::string sendHttpsRequest(ssl_socket& socket, tcp::resolver& resolver, boost::asio::streambuf& request, const char* server);
+void openSslSocket(ssl_socket& sock, tcp::resolver& resolver, const char* hostname);
+std::string sendHttpsRequest(ssl_socket& socket, tcp::resolver& resolver, boost::asio::streambuf& request, const char* hostname);
 
-void openTcpSocket(tcp::socket& sock, tcp::resolver& resolver, const char* server, const char* port);
+void openTcpSocket(tcp::socket& sock, tcp::resolver& resolver, const char* hostname, const char* port);
 std::vector<char> sendTcpRequest(tcp::socket& socket, std::vector<char>& request);
 
-std::vector<char> sendUdpRequest(udp::socket& socket, udp::resolver& resolver, std::vector<char> request, const char* server, const char* port = "80");
+std::vector<char> sendUdpRequest(udp::socket& socket, udp::resolver& resolver, std::vector<char> request, const char* hostname, const char* port = "80");
