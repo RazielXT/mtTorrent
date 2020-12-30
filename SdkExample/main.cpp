b9ef428d3c423cc1aa4333e4263d87cba4b56014
RazielXT <marektomco@gmail.com>
2020-12-30 11:51:29 +0100

    SdkExample: add second bigger example

--- SdkExample/main.cpp
+++ SdkExample/main.cpp
@@ -1,5 +1,138 @@
 #include "Api\Core.h"
 #include "Api\Configuration.h"
+#include <thread>         // std::this_thread::sleep_for
+#include <chrono>
+#include <iostream>
+
+void example2()
+{
+	auto core = mttApi::Core::create();
+
+	auto[status, torrent] = core->addFile("path/to/torrent/file.torrent");
+
+	if (torrent)
+	{
+		torrent->start();
+
+		while (!torrent->finished())
+		{
+			std::cout << torrent->name() << " progress: " << torrent->currentProgress() * 100 << "%%\n";
+			std::this_thread::sleep_for(std::chrono::seconds(1));
+		}
+	}
+	else
+	{
+		std::cout << "Adding torrent failed with error " << (int)status;
+		return;
+	}
+
+	auto& torrentFile = torrent->getFileInfo();
+
+	if (!torrentFile.about.createdBy.empty())
+		std::cout << "Torrent created by " << torrentFile.about.createdBy;
+
+	for (auto& file : torrentFile.info.files)
+	{
+		std::cout << "File: " << file.path.back() << std::endl;
+		std::cout << "File size: " << file.size << std::endl;
+
+		//subdirectory
+		if (file.path.size() > 1)
+		{
+			std::string fileDirectory;
+			for (size_t i = 0; i < file.path.size() - 1; i++)
+				fileDirectory += file.path[i] + "\\";
+
+			std::cout << "Directory: " << fileDirectory << std::endl;
+		}
+	}
+
+	std::cout << "Torrent divided into " << torrentFile.info.pieces.size() << " pieces, with piece size " << torrentFile.info.pieceSize << std::endl;
+
+	auto selection = torrent->getFilesSelection();
+
+	std::vector<bool> newSelection;
+	std::vector<mtt::Priority> newPriority;
+
+	for (auto s : selection.files)
+	{
+		newSelection.push_back(!s.selected);
+		newPriority.push_back(mtt::Priority::High);
+	}
+
+	torrent->selectFiles(newSelection);
+	torrent->setFilesPriority(newPriority);
+	torrent->setLocationPath("path\\to\\new\\download\\path");
+
+	if (!torrent->selectionFinished())
+		std::cout << "Selection progress: " << torrent->currentSelectionProgress() * 100 << "%%\n";
+
+	auto transfer = torrent->getFileTransfer();
+
+	std::cout << "Current download speed: " << transfer->getDownloadSpeed() << " bytes per second" << std::endl;
+
+	auto peers = transfer->getPeersInfo();
+
+	std::cout << "Currently connected to " << peers.size() << " peers" << std::endl;
+
+	for (auto& peer : peers)
+		std::cout << "Address: " << peer.address << ", speed " << peer.downloadSpeed << ", torrent client" << peer.client << std::endl;
+
+	std::cout << "Currently downloading " << transfer->getCurrentRequests().size() << " pieces" << std::endl;
+
+	if (auto magnet = torrent->getMagnetDownload())
+	{
+		mtt::MetadataDownloadState state = magnet->getState();
+
+		if(!state.finished && state.partsCount)
+			std::cout << "Metadata  progress " << state.receivedParts << "/" << state.partsCount << std::endl;
+
+		std::vector<std::string> logs;
+		if (auto count = magnet->getDownloadLog(logs, 0))
+		{
+			for (auto& l : logs)
+				std::cout << "Metadata log: " << l << std::endl;
+		}
+	}
+
+	{
+		auto peers = torrent->getPeers();
+
+		std::cout << "Found peers " << peers->receivedCount() << std::endl;
+		std::cout << "Connected peers " << peers->connectedCount() << std::endl;
+
+		auto sources = peers->getSourcesInfo();
+		for (auto source : sources)
+		{
+			if(source.state >= mtt::TrackerState::Connected)
+				std::cout << source.hostname << " has " << source.peers << " peers" << std::endl;
+		}
+
+		peers->refreshSource(sources.front().hostname);
+
+		//connect locally running client
+		peers->connect("127.0.0.1:55555");
+	}
+
+	core->registerAlerts((int)mtt::AlertId::MetadataFinished | (int)mtt::AlertId::TorrentAdded);
+
+	//...
+
+	auto alerts = core->popAlerts();
+	for (auto& alert : alerts)
+	{
+		if (alert->id == mtt::AlertId::MetadataFinished)
+		{
+			auto hash = alert->getAs<mtt::MetadataAlert>()->hash;
+			std::cout << core->getTorrent(hash)->name() << " finished" << std::endl;
+		}
+		else if (alert->id == mtt::AlertId::TorrentAdded)
+		{
+			auto hash = alert->getAs<mtt::TorrentAlert>()->hash;
+			std::cout << core->getTorrent(hash)->name() << " added" << std::endl;
+		}
+	}
+}
 
 #include <windows.h>
 
@@ -10,7 +143,7 @@ int main()
 	auto settings = mtt::config::getExternal();
 	settings.files.defaultDirectory = "./";
 	mtt::config::setValues(settings.files);
-	settings.dht.enable = true;
+	settings.dht.enabled = true;
 	mtt::config::setValues(settings.dht);
 
 	core->registerAlerts((int) mtt::AlertId::MetadataFinished);
