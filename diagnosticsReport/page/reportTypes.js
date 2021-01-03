7e318c576b1ceb58d8ec76d150932b4af786fea8
RazielXT <marektomco@gmail.com>
2021-01-03 11:38:08 +0100

    Diagnostics: add report page resources

---
+++ diagnosticsReport/page/reportTypes.js
@@ -0,0 +1,12 @@
+//Connect, Connected, RemoteConnected, Disconnect, ReceiveMessage, Interested, Choke, RequestPiece, Piece
+const PeerEvent = {
+    	Connect: 0,
+    	Connected: 1,
+    	RemoteConnected: 2,
+	Disconnect: 3,
+	ReceiveMessage: 4,
+    	Interested: 5,
+    	Choke: 6,
+    	RequestPiece: 7,
+	Piece: 8
+}
